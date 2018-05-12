1573492c87dedadf5f55bb95864bf7487908b856
embaz159 <36550993+embaz159@users.noreply.github.com>
2018-05-12 10:39:19 +0100

    Delete Add Two Numbers.js

--- Add Two Numbers.js
+++
@@ -1,8 +0,0 @@
-// Write a function that takes in two numbers and then adds them together.
-
-function addTwonumbers (num1, num2){
-    return num1 + num2;
-   
-   }
-   
-   console.log(addTwonumbers(5, 6));
