4d33d2270bb42c1b7dad242ec8a973edc38b0724
embaz159 <embaz159@gmail.com>
2018-05-12 10:14:06 +0100

    addTwoNumbers algorithm and readme updated

---
+++ Add Two Numbers/Add Two Numbers.js
@@ -0,0 +1,8 @@
+// Write a function that takes in two numbers and then adds them together.
+
+function addTwonumbers (num1, num2){
+    return num1 + num2;
+   
+   }
+   
+   console.log(addTwonumbers(5, 6));
